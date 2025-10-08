931ca325c5b14551e502d58183a2a5cf3f27ada3
rakanunith <165897979+rakanunith@users.noreply.github.com>
2025-10-08 09:52:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rakanunith/streamavatarcreator/',   // ⬅️ IMPORTANT: replace with your repo, e.g. /unith-avatar-app/
+  base: '/streamavatarcreator/',   // ⬅️ IMPORTANT: replace with your repo, e.g. /unith-avatar-app/
   plugins: [react()],
   server: { port: 5175 },
 })
